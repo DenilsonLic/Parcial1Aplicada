b22b60805ff3e4249950cc7bd639a7291c5ae828
DenilsonLic <100452083+DenilsonLic@users.noreply.github.com>
2022-09-06 17:37:16 -0600

    update

--- Parcial3.UI/frmMunicipio.Designer.cs
+++ Parcial3.UI/frmMunicipio.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comboBoxMunicipios = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxMunicipios
+            // 
+            this.comboBoxMunicipios.FormattingEnabled = true;
+            this.comboBoxMunicipios.Location = new System.Drawing.Point(42, 63);
+            this.comboBoxMunicipios.Name = "comboBoxMunicipios";
+            this.comboBoxMunicipios.Size = new System.Drawing.Size(121, 23);
+            this.comboBoxMunicipios.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 300);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(785, 150);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // frmMunicipio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBoxMunicipios);
+            this.Name = "frmMunicipio";
             this.Text = "frmMunicipio";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private ComboBox comboBoxMunicipios;
+        private DataGridView dataGridView1;
     }
 }
